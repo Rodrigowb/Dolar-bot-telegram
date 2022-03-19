2fbbbf656be2116b4f8a1a81552a5783aa734855
Rodrigo Wanderley <69699102+Rodrigowb@users.noreply.github.com>
2022-03-19 09:22:08 -0400

    Update mando_bot.py
    Adding comments

--- mando_bot.py
+++ mando_bot.py
@@ -1,13 +1,3 @@
-# Create a group with the bot
-# https://api.telegram.org/bot1974352465:AAEDfX7hlppFDknepdwu4-dL1U4O787HV7M/getUpdates
-# Chat ID: -551925848
-# Create a bot @BotFather
-# Send_info_message
-# https://api.telegram.org/bot1974352465:AAEDfX7hlppFDknepdwu4-dL1U4O787HV7M/sendMEssage?chat_id=-551925848&text='Hi, I am Mandalorian and this is the way!
-# requests.get(base_url)
-# Send_error_message
-# https://api.telegram.org/bot1968918950:AAFnE98tE42nSaloofevSn6oFLlkWEPoKAs/sendMEssage?chat_id=-533397331&text=''
-
 import requests
 from pandas_datareader import data as pdr
 from datetime import datetime, date
@@ -27,8 +17,8 @@ class DolarUpdates:
     def send_error_message(text):
         """
 
-        :param text: texto para ser enviado ao grupo de errors telegram
-        :return: mensagem de erro no grupo de erro telegram
+        :param text: text to be send in the Telegram group
+        :return: error message
         """
         dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         requests.get(
@@ -40,8 +30,8 @@ class DolarUpdates:
     def send_info_message(text):
         """
 
-        :param text: texto para ser enviado ao grupo de infos do telegram
-        :return: mensagem de info no grupo de info do telegram
+        :param text: text to be send in the Telegram group
+        :return: information method
         """
         dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         requests.get(
@@ -52,7 +42,7 @@ class DolarUpdates:
     def get_df(self):
         """
 
-        :return: retorna dolar dataframe
+        :return: price Data Frame
         """
         try:
             dolar_df = pdr.get_data_yahoo(self.ticker)
@@ -69,8 +59,7 @@ class DolarUpdates:
     def execute(self, event=None, context=None):
         """
 
-        :return: envia mensagem no telegram com o preço atual, abertura
-        e variacao %
+        :return: send Telegram message with open price, current price and variation
         """
         dolar_df = DolarUpdates.get_df(self)
         if (self.today_day not in self.sp_holidays) and (self.today_day.weekday() < 5):
